92486141a1ac523efdf1b7ba10a9da3bbb709693
16-Bit-Dog <67922228+16-Bit-Dog@users.noreply.github.com>
2023-05-06 12:32:51 -0400

    commented clean code

--- handtrack.py
+++ handtrack.py
@@ -9,8 +9,7 @@ import math
 #program works best with a flat hand to point at objects due to AI reading hand better for depth -- works best when it sees wrist
 #works best closer 
 
-#TODO:remove log 10 from z calc
-#Async work on average logic for loc depth and such for Parkinson's pointer feature 
+#Async work on average logic for loc depth and such for Parkinson's pointer feature?
 #TODO: figure out that crash  File "c:\CodeRepos\BradikenisticPointer\BradikenisticPointer\handtrack.py", line 58, in getZfromDepth
         #return self.currDepth[cy][cx]
         #IndexError: index 384 is out of bounds for axis 0 with size 384
@@ -51,6 +50,8 @@ class handTracker():
         self.model = None
         self.device = None
 
+        cudaFound = False
+
     def getWidthOfCurrImage(self):
         return int(len(self.currImage[0]))
     def getHeightOfCurrImage(self):
@@ -62,9 +63,9 @@ class handTracker():
         if(cy >= int(len(self.currDepth)) or cx>=int(len(self.currDepth[int(cy)]))):
             return -1
         else:
-            #print(len(self.currDepth[cy]))
             return self.currDepth[int(cy)][int(cx)]
 
+    #draw fail text in middle of screen if pointer finds nothing by finding something off screen
     def DrawFailCode(self):
         cv2.putText(self.currImage, "No Hit", (int(self.getWidthOfCurrImage()/2), int(self.getHeightOfCurrImage()/2)), 1, 1,(255,0,0), 2, cv2.LINE_AA)
 
@@ -72,29 +73,33 @@ class handTracker():
         cx = self.nailPosX
         cy = self.nailPosY
         cz = self.nailPosZ
-        #in body
+        
         while(True):
+            # Person is pointing off screen in the X direction
             if(cx<0 or cx >= self.getWidthOfCurrImage()):
                 print("failed X")
                 return None
+            # Person is pointing off screen in the Y direction
             elif(cy<0 or cy >= self.getHeightOfCurrImage()):
                 print("failed Y")
                 return None
-            elif(cz < self.getZfromDepth(cx,cy) and cz < self.nailPosZ-(abs(self.nailPosZ-self.wristZ))):
+            #following code checks if your tracker position is behind the finger 
+            elif(cz < self.getZfromDepth(cx,cy) and cz < self.nailPosZ-(abs(self.nailPosZ-self.wristZ))): #pushes nail backwards based on distance from base of finger to nail, this helps stop the moving 1 step and hitting same finger the tracker spawned from
                 cv2.circle(self.currImage,(int(cx),int(cy)), 10 , (255,50,0), cv2.FILLED) #HIT OBJECT, DO STUFF
                 return 1
-            else:
+            # Have not hit object yet, update current position
+            else: 
                 cx+=(self.fingerDirVecX)
                 cy+=(self.fingerDirVecY)
                 cz+=(-1*(abs(self.fingerDirVecZ))) #incase you have hand over predicted hand location -- need -
                 
 
 
-    def GetDepthNew(self): #REMEMBER: run on GPU AS LONG AS POSSABLE, conevert to cpu minimal --   torch.cuda.synchronize() to test if gpu slow since .cpu() is a sync point -- use manual         torch.cuda.synchronize()
+    def GetDepthNew(self): #REMEMBER: run on GPU AS LONG AS able, convert to cpu minimal -- torch.cuda.synchronize() to test if gpu slow since .cpu() is a sync point -- use manual         torch.cuda.synchronize()
         pixel_values = self.feature_extractor(self.currImage, return_tensors="pt").pixel_values
         with torch.no_grad(): 
-            outputs = self.model(pixel_values.to(self.device)) 
-            predicted_depth = outputs.predicted_depth
+            outputs = self.model(pixel_values.to(self.device))  #this is the slowest part of the code as profiled
+            predicted_depth = outputs.predicted_depth 
         # interpolate to original size
         prediction = torch.nn.functional.interpolate( # is expression does not run
                             predicted_depth.unsqueeze(1),
@@ -103,12 +108,13 @@ class handTracker():
                             mode="bicubic",
                             align_corners=False,
                     ).squeeze()
-        outputGPU = prediction.cuda()
-        #print("a")
-        output = outputGPU.detach().cpu().numpy() 
-        #print("b")
+        #allow non CUDA for debug builds, note: it is SLOW
+        if self.cudaFound == True:
+            outputGPU = prediction.cuda()
+            output = outputGPU.detach().cpu().numpy() 
+        else:
+            output = predicted_depth.cpu().numpy()
         formatted = (output * 255 / np.max(output)).astype('uint8')
-        #print("c")
         return formatted
 
     def handsFinder(self,image,draw=True):
@@ -128,11 +134,13 @@ class handTracker():
         foundBase = False
         if self.results.multi_hand_landmarks:
             Hand = self.results.multi_hand_landmarks[handNo]
-            for id, lm in enumerate(Hand.landmark): #we only need pos 8, and 7-5 avg
+            for id, lm in enumerate(Hand.landmark): #enumerate all hand objects found
                 h,w,c = image.shape
                 cx,cy = int(lm.x*w), int(lm.y*h)
                 if(cx>=self.getWidthOfCurrImage() or cy >= self.getHeightOfCurrImage()): #Making sure not off right side of screen
                     return
+                
+                #only work on nail and finger body -- id 8 & 6, if found draw a circle
                 if id == 8:
                     self.nailPosX = cx
                     self.nailPosY = cy
@@ -149,60 +157,67 @@ class handTracker():
                         cv2.circle(image,(cx,cy), 15 , (255,0,255), cv2.FILLED)
                     foundBase = True
                     
-            
+            #gets direction vector. 
             self.fingerDirVecX = (self.nailPosX - self.wristX)/10
             self.fingerDirVecY = (self.nailPosY - self.wristY)/10
             self.fingerDirVecZ = (self.nailPosZ - self.wristZ)/10
+            #
+            #if AI model bugs out then the dirvecZ will be over 100 and need pruning 
             if(self.fingerDirVecZ>10):
                 self.fingerDirVecZ= self.fingerDirVecZ/10
+            #commented out normalization code since this method is generally less accurate
             #length = (self.fingerDirVecX**2+self.fingerDirVecY**2+self.fingerDirVecZ**2)**0.5
             #self.fingerDirVecX = self.fingerDirVecX/length  
             #self.fingerDirVecY = self.fingerDirVecY/length 
             #self.fingerDirVecZ = self.fingerDirVecZ/length 
-            
-            print((self.fingerDirVecX,self.fingerDirVecY,self.fingerDirVecZ))
-            
-            #if foundBase == False:
-            #    self.wristX = -1
-            #    self.wristY = -1
-            #if foundNail == False:
-            #    self.nailPosX = -1
-            #    self.nailPosY = -1 
-                
-
-
-
 
 
 def main():
     #Launch video capture from standard position of computer camera.
     cap = cv2.VideoCapture(0)
     tracker = handTracker()
+    
+    #large model -- commented out since slower
+    #tracker.feature_extractor = DPTFeatureExtractor.from_pretrained("Intel/dpt-large")
+    #tracker.model = DPTForDepthEstimation.from_pretrained("Intel/dpt-large")
+    #
+
+    #lighter depth model
     tracker.feature_extractor = DPTFeatureExtractor.from_pretrained("Intel/dpt-hybrid-midas")
-    tracker.model = DPTForDepthEstimation.from_pretrained("Intel/dpt-hybrid-midas", low_cpu_mem_usage=True)    
-    tracker.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print(tracker.device)
+    tracker.model = DPTForDepthEstimation.from_pretrained("Intel/dpt-hybrid-midas", low_cpu_mem_usage=True)
+    #
+    #get computational device -- prefer cuda
+    if torch.cuda.is_available:
+        tracker.device = torch.device("cuda:0")
+        tracker.cudaFound = True
+    else:
+        tracker.device = torch.device("cpu")
+
+    #set computational device
     tracker.model.to(tracker.device)
     
-    #Hold camera on while window is running.
-    #Note: Previous image is tracked to do the depth disparity.
-    #Calculate current 'grey' earlier in doing so. (self.gray* variables to do it)
+    #program main loop
     while True:
+
         success,tracker.currImage = cap.read()
+
+        #uses model to find hand
         tracker.currImage = tracker.handsFinder(tracker.currImage)
         
-        #depth getter code
+        #depth image computer function call
         tracker.currDepth = tracker.GetDepthNew()
 
+        #finds hand point positions
         tracker.positionFinder(tracker.currImage)
         
+        #if invalid locations draw out of bound finger location-- else do image walker
         if tracker.nailPosX == -1 or tracker.wristX == -1:
             tracker.DrawFailCode()
         elif tracker.walkTillHit() == None:
             tracker.DrawFailCode()
 
+        #draw images
         cv2.imshow("Video",tracker.currImage)
-        
         cv2.imshow("Depth", tracker.currDepth)
         
         cv2.waitKey(1)
